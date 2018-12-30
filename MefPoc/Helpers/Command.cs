1e455687cfedcba3c88f8c73ef9dbc48d9c1c365
Crabcat <zdw1616@yahoo.com>
2018-12-29 17:14:56 -0800

    Add project files.

---
+++ MefPoc/Helpers/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MefPoc
+{
+    public class Command : ICommand
+    {
+        public Command(Action<object> executeThis)
+            : this(executeThis,(o) => { return true; })
+        {
+        }
+
+        public Command(Action<object> executeThis, Func<object,bool> canExecuteThis)
+        {
+            _execute = executeThis;
+            _canExecute = canExecuteThis;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        // Private members
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+ 
+    }
+}
---
+++ MefPoc/Helpers/ModelViewModelBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MefPoc
+{
+    public abstract class ModelViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raise a PropertyChanged event whenever a property changes
+        /// </summary>
+        /// <param name="target">The property that changed</param>
+        protected void RaisePropertyChangedEvent(object target)
+        {
+            // Check for event listeners
+            if(PropertyChanged != null)
+            {
+                //Fire the event
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(target)));
+            }
+        }
+    }
+}
